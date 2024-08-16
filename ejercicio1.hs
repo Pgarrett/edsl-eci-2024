ed87fce0658f5d2863cc8ab006e40a655ea3c49f
Pgarrett <philip.garrett@mulesoft.com>
2024-08-16 09:19:04 -0300

    tests for exercise 1, not working

--- ejercicio1.hs
+++ ejercicio1.hs
@@ -22,31 +22,80 @@ instance Expr Eval where
     and (E b) (E c) = E (b && c)
     or (E b) (E c)  = E (b || c)
 
--- tests :: IO Counts
--- tests = do runTestTT allTests
 
--- allTests = test [
---     "testValSavesInput" ~: testValSavesInput    
---     ]
+tests :: IO Counts
+tests = do runTestTT allTests
 
--- v3 :: Eval Int
--- v3 = val 3
+allTests = test [
+    "testValSavesInput" ~: testValSavesInput,
+    "testSameValuesAreEqual" ~: testSameValuesAreEqual,
+    "testDifferentValuesAreNotEqual" ~: testDifferentValuesAreNotEqual,
+    "test3IsLowerThan4" ~: test3IsLowerThan4,
+    "test5IsNotLowerThan4" ~: test5IsNotLowerThan4,
+    "testNot3IsLowerThan4ReturnsFalse" ~: testNot3IsLowerThan4ReturnsFalse,
+    "testNot5IsLowerThan4ReturnsTrue" ~: testNot5IsLowerThan4ReturnsTrue,
+    "testAndTrueFalseReturnsFalse" ~: testAndTrueFalseReturnsFalse,
+    "testAndTrueTrueReturnsTrue" ~: testAndTrueTrueReturnsTrue,
+    "testOrTrueFalseReturnsTrue" ~: testOrTrueFalseReturnsTrue,
+    "testOrFalseFalseReturnsFalse" ~: testOrFalseFalseReturnsFalse
+    ]
 
--- testValSavesInput = test [
---     v3 ~=? 3
---     ]
 
+testValSavesInput = test [
+    (val 3) ~=? 3    
+    ]
 
--- tests :: IO Counts
--- tests = do runTestTT allTests
+testSameValuesAreEqual = test [
+    (eq (val 3) (val 3)) ~=? True
+    ]
 
--- allTests = test [
---   "ejercicio1" ~: testsEj1,
---   "ejercicio2" ~: testsEj2,
---   "ejercicio3" ~: testsEj3,
---   "ejercicio4" ~: testsEj4,
---   "ejercicio5" ~: testsEj5,
---   "ejercicio6" ~: testsEj6,
---   "auxiliar-apareceIgualCantidadDeVeces" ~: testsApareceIgualCantidadDeVeces,
---   "auxiliar-contarOcurrenciasTupla" ~: testsContarOcurrenciasTupla
---   ]
+testDifferentValuesAreNotEqual = test [
+    (eq (val 3) (val 4)) ~=? False
+    ]
+
+test3IsLowerThan4 = test [
+    (lt (val 3) (val 4)) ~=? True
+    ]
+
+test5IsNotLowerThan4 = test [
+    (lt (val 5) (val 4)) ~=? False
+    ]
+
+testNot3IsLowerThan4ReturnsFalse = test [
+    (not (lt (val 3) (val 4))) ~=? False
+    ]
+
+testNot5IsLowerThan4ReturnsTrue = test [
+    (not (lt (val 5) (val 4))) ~=? True
+    ]
+
+testAndTrueFalseReturnsFalse = test [
+    (and (lt (val 3) (val 4)) (lt (val 5) (val 4))) ~=? False
+    ]
+
+testAndTrueTrueReturnsTrue = test [
+    (and (lt (val 3) (val 4)) (lt (val 2) (val 4))) ~=? True
+    ]
+
+testOrTrueFalseReturnsTrue = test [
+    (or (lt (val 3) (val 4)) (lt (val 5) (val 4))) ~=? True
+    ]
+
+testOrFalseFalseReturnsFalse = test [
+    (or (lt (val 7) (val 4)) (lt (val 4) (val 4))) ~=? False
+    ]
+
+-- class TExpr e where
+--     valT :: Int -> e Int
+--     addT ::e Int ->e Int ->e Int 
+--     isZeroT :: e Int -> e Bool
+--     ifT :: e Bool -> e t -> e t -> e t
+
+-- data TEval t = TE t
+--    deriving Show
+   
+-- instance TExpr TEval where
+--     valT x = TE x
+--     addT (TE x)(TE y)=TE (x+y)
+--     isZeroT (TE x) = TE (x == 0)
+--     ifT (TE c)(TE x)(TE y)=TE (if c then x else y)
